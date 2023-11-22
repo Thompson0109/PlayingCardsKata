cfdf8e878cbc052e490e302185435ffd5059423c
louis.thompson <louis@42r.com>
2023-11-22 15:44:34 +0000

    adds two cards values together

---
+++ Playing Cards kata/Models/CardValues.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playing_Cards_kata.Models
+{
+    public class CardValues
+    {
+
+        public static int T = 10;
+        public static int J = 11;  
+
+        
+
+        public static bool isJokerAccepted()
+        {
+
+            return true;
+        }
+    }
+}
--- PlayingCardsKata.Test/UnitTest1.cs
+++ PlayingCardsKata.Test/UnitTest1.cs
@@ -1,14 +1,25 @@
+using Playing_Cards_kata;
+using Playing_Cards_kata.Models;
+
 namespace PlayingCardsKata.Test
 {
     [TestClass]
     public class PlayingCardsKataUnitTests
     {
+        CardGame game = new CardGame();
+        CardValues Cards = new CardValues();
+
+
         //GIVEN I have started the card Game application 
         //WHEN I enter a ‘<listOfCards>’ containing one or two jokers
         //THEN the score for the hand should and displayed on the user interface
         [TestMethod]
         public void JokersInCardList()
         {
+
+           int TestVal =  CardValues.T + CardValues.J;
+            Console.WriteLine(TestVal);
+            Assert.AreEqual(TestVal, 21);
         }
 
         //Given I have started the Card Game application
